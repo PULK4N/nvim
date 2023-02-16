8e9e98dec73c32aab6e611092968719a2f442e9f
NIKOLA <nikola.pupovac@rationaletech.com>
2023-02-16 12:10:11 +0100

    working with mason

--- lua/nikola/lsp/init.lua
+++ lua/nikola/lsp/init.lua
@@ -3,6 +3,7 @@ if not status_ok then
   return
 end
 
-require "nikola.lsp.lsp-settings"
+--[[ require "nikola.lsp.lsp-settings" ]]
+require("nikola.lsp.mason")
 require("nikola.lsp.handlers").setup()
 require "nikola.lsp.null-ls"
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -144,10 +144,15 @@ _G.packer_plugins = {
     path = "/home/nikola/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
-  ["nlsp-settings.nvim"] = {
+  ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/nikola/.local/share/nvim/site/pack/packer/start/nlsp-settings.nvim",
-    url = "https://github.com/tamago324/nlsp-settings.nvim"
+    path = "/home/nikola/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/nikola/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
   },
   ["null-ls.nvim"] = {
     loaded = true,
@@ -168,11 +173,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/nikola/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/nvchad/nvim-colorizer.lua"
-  },
-  ["nvim-lsp-installer"] = {
-    loaded = true,
-    path = "/home/nikola/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
-    url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
