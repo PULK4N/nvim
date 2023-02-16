e3d248cd517f13798d27c6443774b1dda756e958
NIKOLA <nikola.pupovac@rationaletech.com>
2023-02-16 12:06:12 +0100

    modified for project to search for .sln files instead of .git

--- lua/nikola/lsp/lsp-settings.lua
+++ lua/nikola/lsp/lsp-settings.lua
@@ -21,12 +21,6 @@ lsp_installer.on_server_ready(function(server)
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  if server.name == "omnisharp" then
-    local sumneko_opts = require("nikola.lsp.settings.omnisharp")
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
-
-
   -- This setup() function is exactly the same as lspconfig's setup function.
   -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
   server:setup(opts)
